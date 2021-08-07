d7e81c2db2f91b22e18fa84b844815931c1af25a
Lee <37562489+schlingy@users.noreply.github.com>
2021-08-07 14:23:15 -0400

    Add files via upload

---
+++ garf_func.py
@@ -0,0 +1,45 @@
+
+from faker import Faker
+from random import randrange
+
+def random_comic():
+    # do the import fake from faker thing
+    fakey = Faker()
+    seed = randrange(100000000)
+    Faker.seed(seed)
+
+    while(True):
+        chosen_date = fakey.date()
+        year, month, day = chosen_date.split('-')
+        if int(month[0]) == 0:
+            month = month[1]
+            print(month)
+        if int(day[0]) == 0:
+            day = day[1]
+            print(day)
+        if int(year) < 1978:
+            if int(year) == 1978 and int(month) < 6 and int(day) < 19:
+                continue
+        else:
+            break
+    
+    if len(month) == 1:
+        month = "0" + month
+    if len(day) == 1:
+        day = "0" + day
+
+    print("year: " + year)
+    print("month: " + month)
+    print("day: " + day)
+    url = "http://images.ucomics.com/comics/ga/" + year + "/ga" + year[2:] + month + day + ".gif"
+    return url, year, month, day
+
+
+def comic_by_date(date):
+    split_date = date.split("-")
+    year = split_date[0]
+    month = split_date[1]
+    day = split_date[2]
+
+    url = "http://images.ucomics.com/comics/ga/" + year + "/ga" + year[2:] + month + day + ".gif"
+    return url
---
+++ garfield.py
@@ -0,0 +1,41 @@
+import discord
+import os 
+from dotenv import load_dotenv
+from garf_func import random_comic, comic_by_date
+
+from discord.ext import commands 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has joined the chat!')
+
+@bot.command()
+async def randomgarf(ctx):
+    url, year, month, day = random_comic()
+    await ctx.send(year + "-" + month + "-" + day)
+    await ctx.send(url)
+
+@bot.command()
+async def garfondate(ctx, arg):
+    comic = comic_by_date(arg)
+    await ctx.send(comic)
+
+@client.event 
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+bot.run(TOKEN)
+client.run(TOKEN)
