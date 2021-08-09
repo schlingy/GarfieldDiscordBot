6f0d4648ebbfd046c5b8144e9f7f61a25fd7cbf3
Lee <37562489+schlingy@users.noreply.github.com>
2021-08-09 18:13:09 -0400

    Add files via upload

--- garf_func.py
+++ garf_func.py
@@ -16,7 +16,7 @@ def random_comic(first_year, first_month, first_day, sunday_only=False):
         if int(day[0]) == 0:
             day = day[1]
         if int(year) < first_year:
-            if int(year) == first_year and int(month) < first_month and int(day) < first_day:
+            if int(year) == int(first_year) and int(month) < int(first_month) and int(day) < int(first_day):
                 continue
         else:
             break
@@ -29,32 +29,6 @@ def random_comic(first_year, first_month, first_day, sunday_only=False):
     url = "http://images.ucomics.com/comics/ga/" + year + "/ga" + year[2:] + month + day + ".gif"
     return url, year, month, day
 
-# def random_comic():
-    # do the import fake from faker thing
-    # fakey = Faker()
-    # seed = randrange(100000000)
-    # Faker.seed(seed)
-
-    # while(True):
-    #     chosen_date = fakey.date()
-    #     year, month, day = chosen_date.split('-')
-    #     if int(month[0]) == 0:
-    #         month = month[1]
-    #     if int(day[0]) == 0:
-    #         day = day[1]
-    #     if int(year) < 1978:
-    #         if int(year) == 1978 and int(month) < 6 and int(day) < 19:
-    #             continue
-    #     else:
-    #         break
-    
-    # if len(month) == 1:
-    #     month = "0" + month
-    # if len(day) == 1:
-    #     day = "0" + day
-
-    # url = "http://images.ucomics.com/comics/ga/" + year + "/ga" + year[2:] + month + day + ".gif"
-    # return url, year, month, day
 
 def random_sunday_comic():
     # do the import fake from faker thing
