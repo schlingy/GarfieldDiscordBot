6e36cecd37e2063dc7eb44f0dec4b79367755756
Lee <37562489+schlingy@users.noreply.github.com>
2021-08-09 17:50:20 -0400

    Add files via upload

--- garf_func.py
+++ garf_func.py
@@ -1,7 +1,7 @@
 
 from faker import Faker
 from random import randrange
-import datetime from datetime 
+from datetime import datetime 
 
 def random_comic():
     # do the import fake from faker thing
